15be8357522119c87b6811f4b2ab360fe63f31a6
Ahmed AG <ahmed.abugharbia@siriuscom.com>
2019-09-01 18:06:50 -0500

    adding deploy dynamo

--- deploy/deploy_realtime.sh
+++ deploy/deploy_realtime.sh
@@ -9,6 +9,8 @@ chmod +rw $code_dir/*
 zip -r $code_dir/$zip_file $code_dir/*
 
 aws s3 cp $code_dir/$zip_file s3://$cfn_bucket/$zip_file
+rm $code_dir/$zip_file
+
 aws cloudformation deploy --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM \
 	--region us-east-1 \
 	--template-file deploy/deploy_realtime_lambda.yaml \
@@ -18,5 +20,4 @@ aws cloudformation deploy --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM \
 	LambdaS3Object=$zip_file \
 	DynamoTable=$dynamo_table \
 
-rm $code_dir/$zip_file
 	
